5f082fdcec6b65f7d07a22bd6191df9a132bdb78
Benjamin Klaus <klaus@technikum-wien.at>
2021-04-19 11:48:43 +0200

    get only ip v4 address

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,14 +6,15 @@ source /opt/ros/kinetic/setup.bash
 source /home/pi/catkin_ws/devel/setup.bash
 
 export TURTLEBOT3_MODEL=burger
-OWN_IP=$(hostname -I | xargs)
+# see https://stackoverflow.com/questions/8529181/which-terminal-command-to-get-just-ip-address-and-nothing-else
+OWN_IP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 roslaunch turtlebot3burger startup.launch &
 
 while [ ! "$OWN_IP" ]; do
   echo "network interface not up, will try again in 1 second";
   sleep 1;
-  OWN_IP=$(hostname -I | xargs)
+  OWN_IP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 done
 
 pkill roslaunch
