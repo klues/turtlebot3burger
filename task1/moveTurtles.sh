b183e3266f703fef43c1b3faa7ca4adc892f642f
Benjamin Klaus <klaus@technikum-wien.at>
2021-05-26 07:57:30 -0700

    added example for rosservice spawn

--- task1/moveTurtles.sh
+++ task1/moveTurtles.sh
@@ -6,3 +6,5 @@ sleep 3
 # TODO: change / insert your commands
 rostopic pub /turtle1/cmd_vel geometry_msgs/Twist --once -- '[1.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]' &
 rostopic pub /turtle1/cmd_vel geometry_msgs/Twist -r 1 -- '[1.0, 0.0, 0.0]' '[0.0, 0.0, 0.5]' &
+
+rosservice call /spawn "{x: 0.0, y: 0.0, theta: 0.0, name: 'test'}"
