3c32cc9440094ccd8341399630ead823aa8acc3e
Benjamin Klaus <klaus@technikum-wien.at>
2021-11-22 13:08:28 +0100

    only autostart if network connection established

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -9,16 +9,11 @@ export TURTLEBOT3_MODEL=burger
 # see https://stackoverflow.com/questions/8529181/which-terminal-command-to-get-just-ip-address-and-nothing-else
 OWN_IP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
-roslaunch turtlebot3burger startup.launch &
-
 while [ ! "$OWN_IP" ]; do
   echo "network interface not up, will try again in 1 second";
   sleep 1;
   OWN_IP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 done
-
-pkill roslaunch
-sleep 5
 
 export ROS_HOSTNAME=$OWN_IP
 echo $ROS_HOSTNAME
